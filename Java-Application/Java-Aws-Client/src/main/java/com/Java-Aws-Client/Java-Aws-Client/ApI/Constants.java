16caf76ba72c09f7f2d49f035d87f5e7baa7fb5d
Gautham Maraswami <114695075+gmaraswa@users.noreply.github.com>
2023-05-07 20:42:41 -0700

    Update Constants.java

--- Java-Application/Java-Aws-Client/src/main/java/com/Java-Aws-Client/Java-Aws-Client/ApI/Constants.java
+++ Java-Application/Java-Aws-Client/src/main/java/com/Java-Aws-Client/Java-Aws-Client/ApI/Constants.java
@@ -3,9 +3,9 @@ package com.ImageId.ImageId.ApI;
 import com.amazonaws.regions.Regions;
 
 public class Constants {
-    public static final String ACCESSKEY = "AKIAUZP4JGMVM5X62UGD";
+    public static final String ACCESSKEY = "#";
 
-    public static final String SECRETKEY = "WIGe8hlfRB/d/YM1hZ06po+mPXPwXlsAsVIcsfmQ";
+    public static final String SECRETKEY = "#";
     public static final Regions REGION = Regions.US_EAST_1;
 
     public static final String BUCKET_NAME = "imageinputbucket";
